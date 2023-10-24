cffdb4d73808cdd8f75539356cbcc97d74fbd6d6
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-10-24 23:38:08 +0900

    fix: 入力欄の枠を正確に抽出できるよう修正

--- python/TestScript/photolize/slt_ver1/detect.py
+++ python/TestScript/photolize/slt_ver1/detect.py
@@ -1,7 +1,4 @@
-# 成功作（画像全体をエッジ抽出する）
-# diff_color_img.pyをより正確に差分ごとに色分けできるようにしたもの
-# 成功作
-# 連番の色枠付き画像を生成＆対応する赤枠と緑枠を出力＆赤枠と緑枠の座標と幅と高さを出力＆配置の差異判定
+# 成功作（入力画面から入力欄の枠を抽出し、元画像にその枠を重ね合わせ＆枠の座標情などを取得）
 # 参考サイト：https://sosotata.com/spot7differences/
 import cv2
 import os
@@ -19,7 +16,7 @@ if not os.path.exists(output_dir):
     subprocess.call(command, shell=True)
 
 # ファイル名を生成
-output_file_name_A = '1_input.png'
+output_file_name_A = '3_input.png'
 # ファイルパスを作成
 output_file_path_A = os.path.join(output_dir, output_file_name_A)
 
@@ -32,9 +29,9 @@ hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
 # hsv_min = np.array([90, 3, 0])
 # hsv_max = np.array([110, 9, 255])
 
-# 薄水色のHSVの範囲
-hsv_min = np.array([90, 3, 0])
-hsv_max = np.array([110, 9, 255])
+# 薄水色のHSVの範囲# [色相, 彩度, 明度]
+hsv_min = np.array([90, 3, 0]) 
+hsv_max = np.array([110, 20, 255])
 
 # マスク画像を作成
 mask = cv2.inRange(hsv, hsv_min, hsv_max)
@@ -48,10 +45,50 @@ masked_img1_gray = cv2.cvtColor(masked_img1, cv2.COLOR_BGR2GRAY)
 # マスク画像から白い領域の輪郭を検出する
 contours, hierarchy = cv2.findContours(masked_img1_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# 別の画像に輪郭を描画する
-result = np.zeros_like(img1)
-cv2.drawContours(result, contours, -1, color=255, thickness=2)
+selected_contours = []
+for cnt in contours:
+    if cv2.contourArea(cnt)>1000: #数字は試行錯誤が必要
+        epsilon = 0.01*cv2.arcLength(cnt,True) #数字は試行錯誤が必要
+        approx = cv2.approxPolyDP(cnt,epsilon,True)
+        selected_contours.append(approx)
 
+# 別の画像に輪郭を描画する
+# result = np.zeros_like(img1)
+cv2.drawContours(img1, selected_contours, -1, color=255, thickness=2)
+
+# 輪郭に番号を付ける
+font = cv2.FONT_HERSHEY_SIMPLEX # フォントの種類
+font_size = 0.5 # フォントのサイズ
+font_color = (0, 0, 255) # フォントの色（BGR）
+for i, cnt in enumerate(selected_contours):
+    x, y, w, h = cv2.boundingRect(cnt) # 矩形領域の座標と幅と高さを取得
+    cv2.putText(img1, str(i+1), (x+w//2, y+h//2), font, font_size, font_color) # 矩形領域の中心に番号を描画
+
+# # 輪郭から矩形領域の座標情報を取得する
+# rectangles = []
+# for cnt in selected_contours:
+#     x, y, w, h = cv2.boundingRect(cnt) # x, yは左上の座標、w, hは幅と高さ
+#     rectangles.append((x, y, w, h))
+
+# # 矩形領域の座標情報を表示する
+# print(rectangles)
+
+# 輪郭から矩形領域の座標情報を取得する
+rectangles = []
+for cnt in selected_contours:
+    x, y, w, h = cv2.boundingRect(cnt) # x, yは左上の座標、w, hは幅と高さ
+    rectangles.append((x, y, w, h))
+
+# 矩形領域の数をカウント
+num_rectangles = len(rectangles)
+print(f"＜入力欄の数＞: {num_rectangles}個\n")
+
+# 矩形領域の座標情報をひとつずつ表示する
+for i, rect in enumerate(rectangles):
+    print(f"入力欄{i+1}の座標: ({rect[0]}, {rect[1]})")
+    print(f"入力欄{i+1}の幅: {rect[2]}")
+    print(f"入力欄{i+1}の高さ: {rect[3]}")
+    print()
 
 
 # img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -109,7 +146,7 @@ if not os.path.exists(output_dir2):
 output_file_path = os.path.join(output_dir2, output_file_name)
 
 # 画像を保存する
-cv2.imwrite(output_file_path, result)
+cv2.imwrite(output_file_path, img1)
 
 print(f"2つの画像の差異部分に枠をつけたカラー画像を{output_file_path}に保存しました")
 
