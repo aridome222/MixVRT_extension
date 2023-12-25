c1e664b861240978b22c93a5241a9afce3137aa6
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2023-12-26 03:01:53 +0900

    update: htmlをフェーズごとに解析するひな形の作成（途中）

--- python/TestScript/photolize/scraping/gen_bf_html.py
+++ python/TestScript/photolize/scraping/gen_bf_html.py
@@ -6,36 +6,168 @@ from datetime import datetime
 import re
 
 
+# def generate_modified_before_html(diff_file_path):
+#     start_tag = '<body>'
+#     end_tag = '</body>'
+
+#     modified_lines = []
+
+#     with open(diff_file_path, 'r') as file:
+#         lines = file.readlines()
+
+#     inside_body_tag = False  # <body> タグ内かどうかを追跡するフラグ
+#     for line in lines:
+#         # <body> タグを検出
+#         if start_tag in line:
+#             inside_body_tag = True
+
+#         # <body> タグ内の場合のみ処理を適用
+#         if inside_body_tag:
+#             if line.startswith('+'):
+#                 continue
+#             elif line.startswith('-'):
+#                 line = line[1:]  # '-'を取り除く
+
+#                 if '<' in line and '>' in line:
+#                     if "class=" in line:
+#                         class_index = line.find('class=')
+#                         class_end_index = line.find('"', class_index + 7)
+#                         line = line[:class_end_index] + " uniqueClass" + line[class_end_index:]
+#                     else:
+#                         tag_end_index = line.find('>')
+#                         line = line[:tag_end_index] + ' class="uniqueClass"' + line[tag_end_index:]
+
+#         modified_lines.append(line)
+
+#         # </body> タグを検出
+#         if end_tag in line:
+#             inside_body_tag = False
+
+
 def generate_modified_before_html(diff_file_path):
+    # <body> タグ内のスタイル情報を抽出
+    start_body_tag = '<body>'
+    end_body_tag = '</body>'
+    inside_body_tag = False  # <body> タグ内かどうかを追跡するフラグ
+
+    # 削除されたコードと変更されていないコードを格納するリスト
     modified_lines = []
 
+    # テキストファイル読み込み
     with open(diff_file_path, 'r') as file:
-        for line in file:
-            # '+'で始まる行を削除する
-            if line.startswith('+'):
-                continue
-            # '-'で始まる行にスタイルを適用し、'-'を取り除く
-            elif line.startswith('-'):
-                line = line[1:]  # '-'を取り除く
-                # HTMLタグを探し、クラスを追加する
-                if '<' in line and '>' in line:
-                    if "class=" in line:
-                        # class属性の位置を見つける
-                        class_index = line.find('class=')
-                        # class属性の値の終わりを見つける
-                        class_end_index = line.find('"', class_index + 7)
-                        # 新しいクラス名を追加
-                        line = line[:class_end_index] + " uniqueClass" + line[class_end_index:]
-                    else:
-                        # タグの終わりを見つける
-                        tag_end_index = line.find('>')
-                        # class属性を追加
-                        line = line[:tag_end_index] + ' class="uniqueClass"' + line[tag_end_index:]
-                modified_lines.append(line)
-            else:
-                modified_lines.append(line)
+        lines = file.readlines()
+
+    # ファイルの各行に対して、削除コードには事前定義したclassを追加し、
+    # 削除コード＋未変更コードを含むリストを生成
+    for line in lines:
+        if body開始タグであれば:
+            body_flag = True
+            pass
+
+        if style開始タグであれば:
+
+        if body_flag == True:
+            # body内における処理
+            pass
+        else style_flag == True:
 
 
+
+
+
+
+        # '+'で始まる行をスキップ
+        if line.startswith('+'):
+            continue
+        # '-'で始まる行に処理を適用
+        elif line.startswith('-'):
+            line = line[1:]  # '-'を取り除く
+
+            # 行中にタグが存在する場合
+            if '<' in line and '>' in line:
+                # タグ内にclass属性が既にあれば、class=""の中の末尾に事前定義classを挿入
+                if "class=" in line:
+                    class_index = line.find('class=')
+                    class_end_index = line.find('"', class_index + 7)
+                    line = line[:class_end_index] + " uniqueClass" + line[class_end_index:]
+                # タグ内にclass属性が無ければ、終了タグ直前に事前定義classを挿入
+                else:
+                    tag_end_index = line.find('>')
+                    line = line[:tag_end_index] + ' class="uniqueClass"' + line[tag_end_index:]
+
+        # 加工した削除コード or 未変更のコードをリストに追加
+        modified_lines.append(line)
+
+
+# できてはいるが、他の影響を考慮していないやつ
+# def generate_modified_before_html(diff_file_path):
+#     # <body> タグ内のスタイル情報を抽出
+#     start_tag = '<body>'
+#     end_tag = '</body>'
+
+#     # 削除されたコードと変更されていないコードを格納するリスト
+#     modified_lines = []
+
+#     # テキストファイル読み込み
+#     with open(diff_file_path, 'r') as file:
+#         lines = file.readlines()
+
+#     # ファイルの各行に対して、削除コードには事前定義したclassを追加し、
+#     # 削除コード＋未変更コードを含むリストを生成
+#     for line in lines:
+#         # '+'で始まる行をスキップ
+#         if line.startswith('+'):
+#             continue
+#         # '-'で始まる行に処理を適用
+#         elif line.startswith('-'):
+#             line = line[1:]  # '-'を取り除く
+
+#             # 行中にタグが存在する場合
+#             if '<' in line and '>' in line:
+#                 # タグ内にclass属性が既にあれば、class=""の中の末尾に事前定義classを挿入
+#                 if "class=" in line:
+#                     class_index = line.find('class=')
+#                     class_end_index = line.find('"', class_index + 7)
+#                     line = line[:class_end_index] + " uniqueClass" + line[class_end_index:]
+#                 # タグ内にclass属性が無ければ、終了タグ直前に事前定義classを挿入
+#                 else:
+#                     tag_end_index = line.find('>')
+#                     line = line[:tag_end_index] + ' class="uniqueClass"' + line[tag_end_index:]
+
+#         # 加工した削除コード or 未変更のコードをリストに追加
+#         modified_lines.append(line)
+
+            
+# .uniqueClass {
+#             position: relative;
+#             /* 親要素のその他のスタイリング */
+#         }
+
+#         .uniqueClass::before {
+#             content: '';
+#             /* 枠のための内容は空 */
+#             position: absolute;
+#             top: 0;
+#             left: 0;
+#             right: 0;
+#             bottom: 0;
+#             border: 3px solid red;
+#             /* 枠線のスタイル */
+#             z-index: 1;
+#             /* 必要に応じて調整 */
+#         }
+
+#         .uniqueClass::after {
+#             content: '変更箇所です。';
+#             position: absolute;
+#             top: 5px;
+#             /* 上からの距離を微調整 */
+#             left: 5px;
+#             /* 左からの距離を微調整 */
+#             color: red;
+#             font-size: 12px;
+#             z-index: 1;
+#         }
 
     # 保存先ディレクトリを指定
     output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "html_data/")
