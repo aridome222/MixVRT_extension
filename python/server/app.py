324b1eccf0cbbb3c53d1a70b6c27323b70964397
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-01-11 16:38:14 +0900

    add: 差分を表示する用のページの追加

--- python/server/app.py
+++ python/server/app.py
@@ -19,10 +19,11 @@ import shlex
 import logging
 
 # 基本ディレクトリの設定
-base_dir = "python/app/base_dir"
-# 
-diff_dir = "app/diff_dir"
+disp_dir = "app/disp"
+# 絶対パス指定
+disp_dir =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), disp_dir)
 
+diff_dir = "app/diff_dir"
 # 絶対パス指定
 diff_dir =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), diff_dir)
 
@@ -37,6 +38,8 @@ app = Flask(__name__, template_folder=template_folder, static_folder=static_fold
 CORS(app)
 # CORS(app, origins=["http://127.0.0.1:5000"], methods=["GET", "POST"])
 
+
+""" MixVRTによって枠付き処理をした変更前と後のWebページを表示する用のルーティング """
 # 別のカスタムテンプレートフォルダを設定
 template_folder2 = os.path.join(diff_dir, 'modified_html', 'templates')
 # 新しいBlueprintを作成
@@ -52,6 +55,29 @@ def modified_testPage_af():
 
 # Blueprintをアプリケーションに登録
 app.register_blueprint(new_blueprint)
+
+
+""" MixVRTによる視覚的回帰テストの結果を表示する用のWebページ """
+# 別のカスタムテンプレートフォルダを設定
+template_folder3 = os.path.join(disp_dir, 'templates')
+static_folder3 = os.path.join(disp_dir, 'static')
+# 新しいBlueprintを作成
+MixVRT = Blueprint('MixVRT', __name__, template_folder=template_folder3, static_folder=static_folder3)
+print(template_folder3)
+print("----------")
+print(static_folder3)
+print("----------")
+
+@MixVRT.route('/MixVRT_url')
+def MixVRT_url():
+    return render_template('MixVRT_url.html')
+
+@MixVRT.route('/MixVRT_diff')
+def MixVRT_diff():
+    return render_template('MixVRT_diff.html')
+
+# Blueprintをアプリケーションに登録
+app.register_blueprint(MixVRT)
 
 
 def clone_or_pull_repo(repo_url, clone_dir):
