c0ae2527e46f2af39bdd671fe5525f5956711759
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-01-12 11:15:53 +0900

    update: 差分を表示するようのページを修正

--- python/app/disp/static/js/index.js
+++ python/app/disp/static/js/index.js
@@ -1,48 +1,20 @@
-'use strict'; /* 厳格にエラーをチェック */
-
-{ /* ローカルスコープ */
-
-    // DOM取得
-    const tabMenus = document.querySelectorAll('.tab__menu-item');
-    console.log(tabMenus);
-
-    // イベント付加
-    tabMenus.forEach((tabMenu) => {
-        tabMenu.addEventListener('click', tabSwitch);
-    })
-
-    // イベントの処理
-    function tabSwitch(e) {
-
-        // クリックされた要素のデータ属性を取得
-        const tabTargetData = e.currentTarget.dataset.tab;
-        // クリックされた要素の親要素と、その子要素を取得
-        const tabList = e.currentTarget.closest('.tab__menu');
-        console.log(tabList);
-        const tabItems = tabList.querySelectorAll('.tab__menu-item');
-        console.log(tabItems);
-        // クリックされた要素の親要素の兄弟要素の子要素を取得
-        const tabPanelItems = tabList.
-            nextElementSibling.querySelectorAll('.tab__panel-box');
-        console.log(tabPanelItems);
-
-        // クリックされたtabの同階層のmenuとpanelのクラスを削除
-        tabItems.forEach((tabItem) => {
-            tabItem.classList.remove('is-active');
-        })
-        tabPanelItems.forEach((tabPanelItem) => {
-            tabPanelItem.classList.remove('is-show');
-        })
-
-        // クリックされたmenu要素にis-activeクラスを付加
-        e.currentTarget.classList.add('is-active');
-        // クリックしたmenuのデータ属性と等しい値を持つパネルにis-showクラスを付加
-        tabPanelItems.forEach((tabPanelItem) => {
-            if (tabPanelItem.dataset.panel === tabTargetData) {
-                tabPanelItem.classList.add('is-show');
-            }
-        })
-
+function openTab(tabName) {
+    var i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
 
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
 }
+
+// 初期タブの設定
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.tab-button').click();
+});
